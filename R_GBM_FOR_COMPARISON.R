7c08a9809d9eb8beade1fe1da6bc7535f95d0ddf
Mohammad Zeeshan <zeeshan.m.khan@live.com>
2016-11-27 01:18:56 +0100

    Update R_GBM_FOR_COMPARISON.R

--- R_GBM_FOR_COMPARISON.R
+++ R_GBM_FOR_COMPARISON.R
@@ -1,5 +1,4 @@
 
-
 library(ROCR)
 library(gbm)
 
@@ -7,24 +6,30 @@ set.seed(123)
 
 d_train <- read.csv("train-1m.csv")
 d_test <- read.csv("test.csv")
-
 d_train$dep_delayed_15min <- ifelse(d_train$dep_delayed_15min=="Y",1,0)
 d_test$dep_delayed_15min <- ifelse(d_test$dep_delayed_15min=="Y",1,0)
 
+facCols <- c("UniqueCarrier", "Origin","Dest", "Month", "DayofMonth", "DayOfWeek")
+numCols <- c("DepTime","Distance")
+
+for (k in facCols) {
+  d_train[[k]] <- as.factor(d_train[[k]])
+  d_test[[k]] <- as.factor(d_test[[k]])
+}
+
+for (k in numCols) {
+  d_train[[k]] <- as.numeric(d_train[[k]])
+  d_test[[k]] <- as.numeric(d_test[[k]])
+}
 
 system.time({
   md <- gbm(dep_delayed_15min ~ ., data = d_train, distribution = "bernoulli", 
             n.trees = 1000, 
             interaction.depth = 16, shrinkage = 0.01, n.minobsinnode = 1,
-            bag.fraction = 0.5)
+            bag.fraction = 0.5, n.cores = 32)
 })
 
 
 phat <- predict(md, newdata = d_test, n.trees = md$n.trees, type = "response")
 rocr_pred <- prediction(phat, d_test$dep_delayed_15min)
 performance(rocr_pred, "auc")@y.values[[1]]
-
-
-
-
-
